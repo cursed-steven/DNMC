e470588a5bd2b91ccb510679d70ba0fed7eebc47
cursed-steven <imaoka.hiroki@gmail.com>
2023-02-19 18:51:51 +0900

    ひとまず基礎工事だけ

--- js/plugins/CSVN_battleResult.js
+++ js/plugins/CSVN_battleResult.js
@@ -27,5 +27,49 @@
     const script = document.currentScript;
     const param = PluginManagerEx.createParameter(script);
 
+    //-----------------------------------------------------------------------------
+    // Scene_Battle
+
+    const _Scene_Battle_createAllWindows = Scene_Battle.prototype.createAllWindows;
+    /**
+     * 結果ウィンドウ用処理追加
+     */
+    Scene_Battle.prototype.createAllWindows = function () {
+        this.createResultWindow();
+        _Scene_Battle_createAllWindows.call(this);
+    };
+
+    /**
+     * 結果ウィンドウ作成
+     */
+    Scene_Battle.prototype.createResultWindow = function () {
+        const rect = this.resultWindowRect();
+        this._resultWindow = new Window_BattleResult(rect);
+        this.addWindow(this._resultWindow);
+    };
+
+    Scene_Battle.prototype.resultWindowRect = function () {
+        // TODO
+    };
+
+    const _Scene_Battle_createDisplayObjects = Scene_Battle.prototype.createDisplayObjects;
+    /**
+     * 結果ウィンドウ用処理追加
+     */
+    Scene_Battle.prototype.createDisplayObjects = function () {
+        _Scene_Battle_createDisplayObjects.call(this);
+        BattleManager.setResultWindow(this._resultWindow);
+        this._resultWindow.setSpriteset(this._spriteset);
+    };
+
+    //-----------------------------------------------------------------------------
+    // BattleManager
+
+    /**
+     * 表示形式を変更
+     */
+    BattleManager.displayRewards = function () {
+        // TODO
+    };
 
 })();
