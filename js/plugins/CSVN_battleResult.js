8f6dcc0017c5e19ee04fbe44fe9baf18fb9138c2
cursed-steven <imaoka.hiroki@gmail.com>
2023-02-19 18:08:16 +0900

    結果表示プラグイン開発開始

---
+++ js/plugins/CSVN_battleResult.js
@@ -0,0 +1,31 @@
+//=============================================================================
+// RPG Maker MZ - CSVN_battleResult
+// ----------------------------------------------------------------------------
+// (C)2023 cursed_twitch
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// ----------------------------------------------------------------------------
+// Version
+// 1.0.0  2023/02/xx 初版
+// ----------------------------------------------------------------------------
+// [Twitter]: https://twitter.com/cursed_twitch
+//=============================================================================
+
+/*:ja
+ * @target MZ
+ * @plugindesc 戦闘結果表示
+ * @author cursed_twitch
+ * @base CSVN_base
+ * @orderAfter CSVN_base
+ * 
+ * @help CSVN_battleResult.js
+ */
+
+(() => {
+
+    'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
+
+
+})();
