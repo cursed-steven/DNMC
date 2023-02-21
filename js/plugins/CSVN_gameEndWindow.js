ffee168e5e504f28534e8441bdc78352b40fdaad
cursed-steven <imaoka.hiroki@gmail.com>
2023-02-21 23:35:13 +0900

    新規プラグイン追加

---
+++ js/plugins/CSVN_gameEndWindow.js
@@ -0,0 +1,48 @@
+//=============================================================================
+// RPG Maker MZ - CSVN_gameEndWindow
+// ----------------------------------------------------------------------------
+// (C)2023 cursed_twitch
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// ----------------------------------------------------------------------------
+// Version
+// 1.0.0  2023/02/21 初版
+// ----------------------------------------------------------------------------
+// [Twitter]: https://twitter.com/cursed_twitch
+//=============================================================================
+
+/*:ja
+ * @target MZ
+ * @plugindesc ゲーム終了時のウィンドウの幅を設定
+ * @author cursed_twitch
+ * @base CSVN_base
+ * @orderAfter CSVN_base
+ * 
+ * @help CSVN_gameEndWindow.js
+ * 
+ * @param width
+ * @text ウィンドウの幅(px)
+ * @type number
+ * @default 160
+ */
+
+(() => {
+
+    'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
+    const WIDTH = param.width > Graphics.boxWidth ? Graphics.boxWidth : param.width;
+
+    /**
+     * ウィンドウ領域を返す
+     * @returns Rectangle
+     */
+    Scene_GameEnd.prototype.commandWindowRect = function () {
+        const ww = WIDTH;
+        const wh = this.calcWindowHeight(2, true);
+        const wx = (Graphics.boxWidth - ww) / 2;
+        const wy = (Graphics.boxHeight - wh) / 2;
+        return new Rectangle(wx, wy, ww, wh);
+    };
+
+})();
