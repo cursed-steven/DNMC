caacac6f5fb6d71d17724f67aa1f75e7e74da3fb
cursed-steven <imaoka.hiroki@gmail.com>
2023-04-16 13:01:48 +0900

    ドロップアイテムについてはwatchされていなかった問題の修正

--- js/plugins/CSVN_gainItemWatcher.js
+++ js/plugins/CSVN_gainItemWatcher.js
@@ -22,6 +22,14 @@
  * 
  * @help CSVN_gainItemWatcher.js
  * 
+ * @param swId
+ * @text 戦闘後の実行に使うスイッチ
+ * @type switch
+ * 
+ * @param varId
+ * @text 戦闘後の実行に使う変数
+ * @type variable
+ * 
  * @param cevs
  * @text コモンイベントと実行条件
  * @desc それぞれに設定されているスイッチと変数両方の条件が満たされるとイベントが実行(予約)されます。
@@ -76,6 +84,9 @@
     const script = document.currentScript;
     const param = PluginManagerEx.createParameter(script);
 
+    //-------------------------------------------------------------------------
+    // Game_Party
+
     const _Game_Party_gainItem = Game_Party.prototype.gainItem;
     /**
      * アイテム増減後に条件判断を行い、合致したら所定のCEV実行を予約する
@@ -87,6 +98,7 @@
         _Game_Party_gainItem.call(this, item, amount, includeEquip);
 
         if (param.cevs) {
+            let cevIds = [];
             const cevs = param.cevs.filter(c => {
                 return CSVN_conditionChecker.checkCondition(
                     0,
@@ -99,10 +111,42 @@
                 );
             });
 
-            for (const cev of cevs) {
-                $gameTemp.reserveCommonEvent(cev.id);
+            if ($gameParty.inBattle()) {
+                // 戦闘終了後はCEV予約はクリアされてしまうため予約内容を退避
+                for (const cev of cevs) {
+                    cevIds.push(cev.id);
+                }
+                $v.set(param.varId, cevIds.join(","));
+                $s.on(param.swId);
+            } else {
+                for (const cev of cevs) {
+                    CSVN_base.log(`> CEV reserved by gainItemWatcher > ${cev.id}`);
+                    $gameTemp.reserveCommonEvent(cev.id);
+                }
             }
         }
     };
 
+    //-------------------------------------------------------------------------
+    // BattleManager
+
+    const _BattleManager_endBattle = BattleManager.endBattle;
+    /**
+     * ドロップアイテムの処理中に退避したCEVがあれば改めて予約する
+     * @param {number} result 
+     */
+    BattleManager.endBattle = function (result) {
+        _BattleManager_endBattle.call(this, result);
+
+        // 退避されているCEVがあれば改めて予約
+        if ($s.get(param.swId)) {
+            const cevIds = $v.get(param.varId).split(",");
+            for (const cevId of cevIds) {
+                CSVN_base.log(`> CEV reserved by gainItemWatcher > ${cevId}`);
+                $gameTemp.reserveCommonEvent(cevId);
+            }
+            $s.off(param.swId);
+            $v.set(param.varId, 0);
+        }
+    };
 })();
