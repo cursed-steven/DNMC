0e219dc5a06df5791b5d9fa345bbbda4d84ecafb
cursed-steven <imaoka.hiroki@gmail.com>
2023-05-27 13:39:19 +0900

    鑑定屋シーンプラグイン着手

---
+++ project/js/plugins/DNMC_appraisalAndMod.js
@@ -0,0 +1,37 @@
+//=============================================================================
+// RPG Maker MZ - DNMC_appraisalAndMod
+// ----------------------------------------------------------------------------
+// (C)2023 cursed_steven
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// ----------------------------------------------------------------------------
+// Version
+// 1.0.0  2023/06/xx 初版
+// ----------------------------------------------------------------------------
+// [Twitter]: https://twitter.com/cursed_steven
+//=============================================================================
+
+/*:ja
+ * @target MZ
+ * @plugindesc 鑑定・MOD屋のメニューUIと内部処理。
+ * @author cursed_steven
+ * @base CSVN_base
+ * @orderAfter CSVN_base
+ * 
+ * @help DNMC_appraisalAndMod.js
+ */
+
+(() => {
+
+    'use strict';
+    const script = document.currentScript;
+    const param = PluginManagerEx.createParameter(script);
+
+    //-------------------------------------------------------------------------
+    // Scene_Appraisal
+    //
+    // The scene class of the appraisal shop scene.
+
+
+
+})();
